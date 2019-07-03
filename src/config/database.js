0227c37fa10f87ddf8dff83c31ef3a555ad1973b
Heitor Pacheco <heeitorpacheco@gmail.com>
2019-07-03 17:52:55 -0300

    Criação do projeto, cadastro de usuário, alteração e login com token

---
+++ src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+  dialect: 'postgres',
+  host: 'localhost',
+  username: 'postgres',
+  password: 'pg2019',
+  database: 'meetapp',
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+};
