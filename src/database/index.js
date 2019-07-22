8101bbdf692f7aaa5c650e3b2a18628e32b8184f
Heitor Pacheco <heeitorpacheco@gmail.com>
2019-07-22 15:07:16 -0300

    Criado tabela de files e campo avatar_id na tabela de users utilizando associate

--- src/database/index.js
+++ src/database/index.js
@@ -3,18 +3,21 @@ import Sequelize from 'sequelize';
 import databaseConfig from '../config/database';
 
 import User from '../app/models/User';
+import File from '../app/models/File';
 
-const models = [User];
+const models = [User, File];
 
 class Database {
   constructor() {
-    this.connection = new Sequelize(databaseConfig);
-
     this.init();
   }
 
   init() {
-    models.forEach(model => model.init(this.connection));
+    this.connection = new Sequelize(databaseConfig);
+
+    models
+      .map(model => model.init(this.connection))
+      .map(model => model.associate && model.associate(this.connection.models));
   }
 }
 
