b362c99987b9048ddf6db2a61b0dc531f9516249
Heitor Pacheco <heeitorpacheco@gmail.com>
2019-07-23 10:07:57 -0300

    alteraçao para nao permitir que sejam cadastrados meetups com datas passadas

--- src/app/controllers/MeetupController.js
+++ src/app/controllers/MeetupController.js
@@ -1,8 +1,15 @@
+import { isBefore, parseISO } from 'date-fns';
 import Meetup from '../models/Meetup';
 
 class MeetupController {
   async store(req, res) {
     const { title, description, location, date, user_id, file_id } = req.body;
+
+    if (isBefore(parseISO(date), new Date())) {
+      return res
+        .status(400)
+        .json({ error: 'Datas anteriores não são permitidas' });
+    }
 
     const meetup = await Meetup.create({
       title,
