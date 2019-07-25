7508eb130eb28c64c9400f53a73d7b39c8ad4496
Heitor Pacheco <heeitorpacheco@gmail.com>
2019-07-25 15:28:31 -0300

    Validaçao com Yup na criaçao de Meetups

--- src/app/controllers/MeetupController.js
+++ src/app/controllers/MeetupController.js
@@ -1,4 +1,5 @@
 import { isAfter, isBefore, parseISO } from 'date-fns';
+import * as Yup from 'yup';
 import Meetup from '../models/Meetup';
 
 class MeetupController {
@@ -24,6 +25,17 @@ class MeetupController {
   }
 
   async store(req, res) {
+    const schema = Yup.object().shape({
+      title: Yup.string().required(),
+      description: Yup.string().required(),
+      location: Yup.string().required(),
+      date: Yup.date().required(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails' });
+    }
+
     const { title, description, location, date, file_id } = req.body;
 
     if (isBefore(parseISO(date), new Date())) {
